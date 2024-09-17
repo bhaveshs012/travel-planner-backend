b6057a6c387982b854d33eb496b0874cba51ce6c
Bhavesh Sengunthar <bhaveshss012@gmail.com>
2024-09-17 23:30:49 +0530

    Update cookieOptions.js

--- src/utils/cookieOptions.js
+++ src/utils/cookieOptions.js
@@ -1,5 +1,7 @@
 const cookieOptions = {
   httpOnly: true,
+  secure: true, // Only send cookies over HTTPS
+  sameSite: "None", // Allow cross-origin requests to send cookies
 };
 
 export default cookieOptions;
